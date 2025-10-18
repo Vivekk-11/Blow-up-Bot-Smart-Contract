0621912cc60a8ccf8160714387c5ceb7d0dce723
Vivek <chimnanivivek14@gmail.com>
2025-10-18 19:01:06 +0530

    sell tokens

--- programs/token_launchpad/src/lib.rs
+++ programs/token_launchpad/src/lib.rs
@@ -98,6 +98,7 @@ pub mod token_launchpad {
 
         invoke(&create_fee_ix, &[
             ctx.accounts.creator.to_account_info(),
+            ctx.accounts.treasury.to_account_info(),
             ctx.accounts.system_program.to_account_info()
         ])?;
 
@@ -149,9 +150,10 @@ pub mod token_launchpad {
         let signer_seeds = &[seeds];
         
         if fee > 0 {
-            let transaction_fee_ix = transfer(&bonding_curve.key(), &cfg.treasury, fee);
+            let transaction_fee_ix = transfer(&bonding_curve.key(), &ctx.accounts.treasury.key(), fee);
             invoke_signed(&transaction_fee_ix, &[
                 bonding_curve.to_account_info(),
+                ctx.accounts.treasury.to_account_info(),
                 ctx.accounts.system_program.to_account_info()
             ], signer_seeds)?;
         }
@@ -176,6 +178,75 @@ pub mod token_launchpad {
 
         Ok(())
     }
+
+    pub fn sell_tokens(ctx: Context<SellTokens>, tokens_in: u64) -> Result<()> {
+        let bonding_curve = &mut ctx.accounts.bonding_curve;
+        let global_config = &mut ctx.accounts.global_config;
+        let seller = &ctx.accounts.seller;
+        let creator_key = bonding_curve.creator;
+        let seeds : &[&[u8]]= &[b"bonding-curve", creator_key.as_ref(), &[bonding_curve.bump]];
+        let signer_seeds = &[seeds];
+
+        require_gt!(tokens_in, 0, ErrorCode::InvalidProgramExecutable);
+
+        let initial_sol_reserves = bonding_curve.virtual_sol_reserves;
+        let initial_token_reserves = bonding_curve.virtual_token_reserves;
+        let sol_out  = calculate_sol_out(tokens_in, initial_sol_reserves, initial_token_reserves);
+
+        let fee_bps = global_config.sell_fee_bps as u128;
+        let sol_128 = sol_out as u128;
+        let fee_128 = fee_bps
+            .checked_mul(sol_128)
+            .ok_or(ErrorCode::InvalidNumericConversion)?
+            .checked_div(10_000u128)
+            .ok_or(ErrorCode::InvalidNumericConversion)?;
+        let fee = fee_128 as u64;
+        let final_sol = sol_out
+            .checked_sub(fee)
+            .ok_or(ErrorCode::InvalidNumericConversion)?;
+
+        let token_transfer_accounts = token::Transfer {
+            from: ctx.accounts.seller_token_account.to_account_info(),
+            to: ctx.accounts.bonding_curve_token_account.to_account_info(),
+            authority: seller.to_account_info()
+        };
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token_transfer_accounts
+        );
+
+        token::transfer(cpi_ctx, tokens_in)?;
+        
+        if bonding_curve.to_account_info().lamports() < sol_out {
+            return err!(ErrorCode::InvalidProgramExecutable);
+        }
+
+        if fee > 0 {
+            let transfer_fee_ix = transfer(&bonding_curve.key(), &ctx.accounts.treasury.key(), fee);
+
+            invoke_signed(&transfer_fee_ix, &[
+                bonding_curve.to_account_info(),
+                ctx.accounts.treasury.to_account_info(),
+                ctx.accounts.system_program.to_account_info()
+            ], signer_seeds)?;
+        }
+
+        let transfer_sol_ix = transfer(&bonding_curve.key(), &seller.key(), final_sol);
+
+        invoke_signed(&transfer_sol_ix, &[
+            bonding_curve.to_account_info(),
+            seller.to_account_info(),
+            ctx.accounts.system_program.to_account_info()
+        ], signer_seeds)?;
+
+        bonding_curve.real_token_reserves = bonding_curve.real_token_reserves.checked_add(tokens_in).ok_or(ErrorCode::InvalidNumericConversion)?;
+        bonding_curve.real_sol_reserves = bonding_curve.real_sol_reserves.checked_sub(sol_out).ok_or(ErrorCode::InvalidNumericConversion)?;
+
+        global_config.total_volume_sol = global_config.total_volume_sol.checked_add(final_sol as u128).ok_or(ErrorCode::InvalidNumericConversion)?;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -190,6 +261,12 @@ pub struct CreateToken<'info> {
     )]
     pub global_config: Account<'info, GlobalConfig>,
 
+    #[account(
+        mut,
+        constraint = treasury.key() == global_config.treasury @ ErrorCode::InvalidProgramExecutable
+    )]
+    pub treasury: AccountInfo<'info>,
+    
     #[account(
         init, 
         payer = creator,
@@ -238,6 +315,12 @@ pub struct BuyTokens<'info> {
 
     #[account(
         mut,
+        constraint = treasury.key() == global_config.treasury @ ErrorCode::InvalidProgramExecutable
+    )]
+    pub treasury: AccountInfo<'info>,
+
+    #[account(
+        mut,
         seeds = [b"bonding-curve", bonding_curve.creator.as_ref()],
         bump = bonding_curve.bump
     )]
@@ -256,6 +339,43 @@ pub struct BuyTokens<'info> {
     pub token_program: Program<'info, Token>
 }
 
+#[derive(Accounts)]
+pub struct SellTokens<'info> {
+    #[account(mut)]
+    pub seller: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"bonding-curve", bonding_curve.creator.as_ref()],
+        bump = bonding_curve.bump
+    )]
+    pub bonding_curve: Account<'info, BondingCurve>,
+
+    #[account(
+        mut,
+        seeds = [b"global-config"],
+        bump = global_config.bump
+    )]
+    pub global_config: Account<'info,  GlobalConfig>,
+    
+    #[account(
+        mut,
+        constraint = treasury.key() == global_config.treasury @ ErrorCode::InvalidProgramExecutable
+    )]
+    pub treasury: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub seller_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub bonding_curve_token_account: Account<'info, TokenAccount>,
+
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+
 pub fn calculate_tokens_out(
     sol_amount: u64,
     initial_sol_reserves: u64,
@@ -267,6 +387,27 @@ pub fn calculate_tokens_out(
     let tokens_out = initial_token_reserves.checked_sub(new_token_reserves).unwrap();
 
     tokens_out
+}
+
+pub fn calculate_sol_out(
+    tokens_in: u64,
+    initial_sol_reserves: u64,
+    initial_token_reserves: u64
+) -> u64 {
+    let k = initial_sol_reserves
+          .checked_mul(initial_token_reserves)
+          .unwrap();
+    let new_token_reserves = initial_token_reserves
+          .checked_add(tokens_in)
+          .unwrap();
+    let new_sol_reserves = k
+          .checked_div(new_token_reserves)
+          .unwrap();
+    let sol_out = initial_sol_reserves
+          .checked_sub(new_sol_reserves)
+          .unwrap();
+
+    sol_out
 }
 
 pub fn initialize_global_config(
